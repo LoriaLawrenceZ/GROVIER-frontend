1c0fec4b5a7f1b4df76d327fe4c44f653b60199c
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-09-04 14:30:17 +0000

    [feat]Analytics  for Vercel
    Signed-off-by: GitHub <noreply@github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={``}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
