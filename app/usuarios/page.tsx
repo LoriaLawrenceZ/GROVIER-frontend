239857b4f66ed3d6982a7a79597ebea24ae821fb
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-09-06 11:50:03 +0000

    [fix] Using 'correct' table to listing users
    Signed-off-by: GitHub <noreply@github.com>

--- app/usuarios/page.tsx
+++
@@ -1,8 +0,0 @@
-  import { createClient } from '@/utils/supabase/server';
-
-  export default async function Usuarios() {
-    const supabase = createClient();
-    const { data: usuarios } = await supabase.from("usuarios").select();
-
-    return <pre>{JSON.stringify(usuarios, null, 2)}</pre>
-  }
